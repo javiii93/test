cd5f0383e992f9d1d7e0eb66cc56f085dd9fa984
Javiii93 <lindejavi93@gmail.com>
2023-05-26 18:23:30 +0200

    main dev

--- src/main/java/es/test/App.java
+++ src/main/java/es/test/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! dev" );
+        System.out.println( "Hello World! devTest" );
     }
 }
